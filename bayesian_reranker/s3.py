65dca4985d131691df4a058f4bf8874aa9414111
Ubuntu <ubuntu@ip-172-31-0-181.us-east-2.compute.internal>
2025-10-12 00:26:03 +0000

    error message

--- bayesian_reranker/s3.py
+++ bayesian_reranker/s3.py
@@ -5,10 +5,15 @@ import json
 bucket = 'sagemaker-us-east-2-344400919253'
 
 def put(path, body):
-    s3 = boto3.client(service_name="s3", aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
-                      aws_secret_access_key=os.environ['AWS_SECRET_KEY'], region_name='us-east-2')
-    s = s3.put_object(Body=json.dumps(body), Bucket=bucket, Key=path)
-
+    try:
+        s3 = boto3.client(service_name="s3", aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+                          aws_secret_access_key=os.environ['AWS_SECRET_KEY'], region_name='us-east-2')
+        s = s3.put_object(Body=json.dumps(body), Bucket=bucket, Key=path)
+    except Exception as e:
+        with open('log.file','a') as f:
+            f.write('put failed', path)
+        return -1
+    return 1
 
 def get(key):
     get_s3 = boto3.client(service_name="s3", aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
